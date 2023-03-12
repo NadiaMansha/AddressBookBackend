4c80864e8179e76cb1bdcee05dc71d57f0b2b341
NadiaMansha <103958651+NadiaMansha@users.noreply.github.com>
2023-03-12 20:41:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,4 @@ app.use('/contacts',require('./routes/contactRoute'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports=app
